bf63cd5f5c7859726909c20ca468ab5a5364e95d
blesson7777 <85436619+blesson7777@users.noreply.github.com>
2026-03-01 00:55:40 +0530

    Update urls.py

--- emianalyzer/urls.py
+++ emianalyzer/urls.py
@@ -5,18 +5,22 @@ from django.core.management import call_command
 from django.http import HttpResponse
 from myapp import views as myapp_views
 
-urlpatterns = [
-    path('admin/', myapp_views.admin_root_redirect, name='admin_root'),
-    path('', include('myapp.urls')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+# Temporary migration function
 def run_migrate(request):
     call_command('migrate')
     return HttpResponse("MIGRATIONS DONE")
 
-urlpatterns += [
+urlpatterns = [
+    # TEMP MIGRATION URL
     path("run-migrate/", run_migrate),
+
+    # Admin root redirect
+    path('admin/', myapp_views.admin_root_redirect, name='admin_root'),
+
+    # All app URLs
+    path('', include('myapp.urls')),
 ]
+
+# Static files (for debug mode only)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
