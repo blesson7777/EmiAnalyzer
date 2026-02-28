c4942fc27e803043928b97906222067a26501058
blesson7777 <85436619+blesson7777@users.noreply.github.com>
2026-03-01 00:45:47 +0530

    Update urls.py

--- emianalyzer/urls.py
+++ emianalyzer/urls.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
-
+from django.core.management import call_command
+from django.http import HttpResponse
 from myapp import views as myapp_views
 
 urlpatterns = [
@@ -11,3 +12,11 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+def run_migrate(request):
+    call_command('migrate')
+    return HttpResponse("MIGRATIONS DONE")
+
+urlpatterns += [
+    path("run-migrate/", run_migrate),
+]
